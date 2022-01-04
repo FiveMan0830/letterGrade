88cf6ca9e6f354d6a622ef5603d183178104b429
Fiveman0830 <t107590013@ntut.org.tw>
2022-01-04 12:35:54 +0800

    1

--- demo/src/test/java/letter/grade/AppTest.java
+++ demo/src/test/java/letter/grade/AppTest.java
@@ -12,47 +12,81 @@ public class AppTest
     /**
      * Input in Partition
      */
-    // @Test
-    // public void inputInFirstPartitionShouldReturnLetterA()
-    // {
-    //     assertEquals('A', App.letterGrade(95));
-    // }
+    @Test
+    public void inputInFirstPartitionShouldReturnLetterA()
+    {
+        assertEquals('A', App.letterGrade(99));
+        assertEquals('A', App.letterGrade(98));
+        assertEquals('A', App.letterGrade(97));
+        assertEquals('A', App.letterGrade(96));
+        assertEquals('A', App.letterGrade(95));
+        assertEquals('A', App.letterGrade(94));
+        assertEquals('A', App.letterGrade(93));
+        assertEquals('A', App.letterGrade(92));
+        assertEquals('A', App.letterGrade(91));
+    }
 
-    // @Test
-    // public void inputInSecondPartitionShouldReturnLetterB()
-    // {
-    //     assertEquals('B', App.letterGrade(83));
-    // }
+    @Test
+    public void inputInSecondPartitionShouldReturnLetterB()
+    {
+        assertEquals('B', App.letterGrade(89));
+        assertEquals('B', App.letterGrade(88));
+        assertEquals('B', App.letterGrade(87));
+        assertEquals('B', App.letterGrade(86));
+        assertEquals('B', App.letterGrade(85));
+        assertEquals('B', App.letterGrade(84));
+        assertEquals('B', App.letterGrade(83));
+        assertEquals('B', App.letterGrade(82));
+        assertEquals('B', App.letterGrade(81));
+    }
 
-    // @Test
-    // public void inputInThirdPartitionShouldReturnLetterC()
-    // {
-    //     assertEquals('C', App.letterGrade(76));
-    // }
+    @Test
+    public void inputInThirdPartitionShouldReturnLetterC()
+    {
+        assertEquals('C', App.letterGrade(79));
+        assertEquals('C', App.letterGrade(78));
+        assertEquals('C', App.letterGrade(77));
+        assertEquals('C', App.letterGrade(76));
+        assertEquals('C', App.letterGrade(75));
+        assertEquals('C', App.letterGrade(74));
+        assertEquals('C', App.letterGrade(73));
+        assertEquals('C', App.letterGrade(72));
+        assertEquals('C', App.letterGrade(71));
+    }
 
-    // @Test
-    // public void inputInFourthPartitionShouldReturnLetterD()
-    // {
-    //     assertEquals('D', App.letterGrade(61));
-    // }
+    @Test
+    public void inputInFourthPartitionShouldReturnLetterD()
+    {
+        assertEquals('D', App.letterGrade(69));
+        assertEquals('D', App.letterGrade(68));
+        assertEquals('D', App.letterGrade(67));
+        assertEquals('D', App.letterGrade(66));
+        assertEquals('D', App.letterGrade(65));
+        assertEquals('D', App.letterGrade(64));
+        assertEquals('D', App.letterGrade(63));
+        assertEquals('D', App.letterGrade(62));
+        assertEquals('D', App.letterGrade(61));
+    }
 
-    // @Test
-    // public void inputInFifthPartitionShouldReturnLetterF()
-    // {
-    //     assertEquals('F', App.letterGrade(35));
-    // }
+    @Test
+    public void inputInFifthPartitionShouldReturnLetterF()
+    {
+        assertEquals('F', App.letterGrade(59));
+        assertEquals('F', App.letterGrade(35));
+        assertEquals('F', App.letterGrade(1));
+    }
 
-    // @Test
-    // public void inputBelowZeroShouldReturnLetterX()
-    // {
-    //     assertEquals('X', App.letterGrade(-3));
-    // }
+    @Test
+    public void inputBelowZeroShouldReturnLetterX()
+    {
+        assertEquals('X', App.letterGrade(-3));
+    }
 
-    // @Test
-    // public void inputOverHundredShouldReturnLetterX()
-    // {
-    //     assertEquals('X', App.letterGrade(1079));
-    // }
+    @Test
+    public void inputOverHundredShouldReturnLetterX()
+    {
+        assertEquals('X', App.letterGrade(1079));
+    }
     
     /**
      * Input On Boundary
