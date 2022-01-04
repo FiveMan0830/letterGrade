2a5f380714c45191007ac96742cbb0636606cca7
Fiveman0830 <t107590013@ntut.org.tw>
2022-01-04 12:47:32 +0800

    change source code

--- demo/src/main/java/letter/grade/App.java
+++ demo/src/main/java/letter/grade/App.java
@@ -26,15 +26,15 @@ public class App
     }
     public static char letterGrade(int score) {
         char grade;
-        if (score <0 || score > 100)
+        if (score < 0 || score > 100)
             grade = 'X';
-        else if (score>=90 && score <=100)
+        else if (score >= 90)
             grade = 'A';
-        else if (score>=80 && score <90)
+        else if (score >= 80)
             grade = 'B';
-        else if (score>=70 && score <80)
+        else if (score >= 70)
             grade = 'C';
-        else if (score>=60 && score <70)
+        else if (score >= 60)
             grade = 'D';
         else
             grade = 'F';
